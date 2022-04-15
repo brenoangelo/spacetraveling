57d90b27f297ae8f840bcc9f3c0feaeea6eee739
Breno Angelo <brenoangeloal@gmail.com>
2022-04-15 19:14:55 -0300

    listing of posts consuming from the prismic api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useEffect, useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -26,54 +30,91 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-const postsList = [
-  {
-    title: 'Como Utilizar hooks',
-    content: 'Pensando em sincronização em vez de ciclos de vida.',
-    date: '15 Mar 2021',
-    author: 'Joseph Oliveira',
-  },
-  {
-    title: 'Como Utilizar hooks',
-    content: 'Pensando em sincronização em vez de ciclos de vida.',
-    date: '15 Mar 2021',
-    author: 'Joseph Oliveira',
-  },
-  {
-    title: 'Como Utilizar hooks',
-    content: 'Pensando em sincronização em vez de ciclos de vida.',
-    date: '15 Mar 2021',
-    author: 'Joseph Oliveira',
-  },
-  {
-    title: 'Como Utilizar hooks',
-    content: 'Pensando em sincronização em vez de ciclos de vida.',
-    date: '15 Mar 2021',
-    author: 'Joseph Oliveira',
-  },
-];
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState('');
 
-export default function Home() {
-  // TODO
+  useEffect(() => {
+    setPosts(postsPagination.results);
+    setPage(postsPagination.next_page);
+  }, []);
+
+  async function handleNextPage() {
+    if (!page) {
+      return;
+    }
+
+    fetch(page)
+      .then(res => res.json())
+      .then(json => {
+        setPage(json.next_page);
+        setPosts(state => [...state, json.results[0]]);
+      });
+  }
+
   return (
-    <main>
-      {postsList.map(post => (
-        <article>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
-          <span>
-            <time><FiCalendar size={20}/> {post.date}</time>
-            <span><FiUser size={20}/> {post.author}</span>
-          </span>
-        </article>
-      ))}
+    <main className={styles.main}>
+      <div className={styles.container}>
+        {posts.map(post => (
+          <article className={styles.postSingle}>
+            <a href="">
+              <h1>{post.data.title}</h1>
+            </a>
+            <p>{post.data.subtitle}</p>
+            <span className={styles.postSingleDetails}>
+              <time>
+                <FiCalendar size={20} />
+                {format(new Date(post.first_publication_date), 'PP', {
+                  locale: ptBR,
+                })}
+              </time>
+              <span>
+                <FiUser size={20} /> {post.data.author}
+              </span>
+            </span>
+          </article>
+        ))}
+
+        <button
+          className={styles.paginationButton}
+          onClick={() => handleNextPage()}
+        >
+          Carregar mais posts
+        </button>
+      </div>
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.content', 'post.author'],
+      pageSize: 1,
+    }
+  );
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: response.next_page,
+        results: posts,
+      },
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +27,56 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  return (
+    <main className={styles.post}>
+      <header>
+        <img src="/images/Banner.png" alt="" />
+      </header>
+      <div className={styles.container}>
+        <div className={styles.postHeader}>
+          <h1>Criando um app CRA do zero</h1>
+          <span className={styles.postSingleDetails}>
+            <time>
+              <FiCalendar size={20} />
+              15 Mar 2021
+            </time>
+
+            <span>
+              <FiUser size={20} /> Joseph Oliveira
+            </span>
+
+            <time>
+              <FiClock size={20} />4 min
+            </time>
+          </span>
+        </div>
+
+        <div className={styles.paragraph}>
+          <h2>Proin et varius</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            dolor sapien, vulputate eu diam at, condimentum hendrerit tellus.
+            Nam facilisis sodales felis, pharetra pharetra lectus auctor sed. Ut
+            venenatis mauris vel libero pretium, et pretium ligula faucibus.
+            Morbi nibh felis, elementum a posuere et, vulputate et erat. Nam
+            venenatis.
+          </p>
+
+          <h2>Proin et varius</h2>
+          <p>
+            Lorem ipsum dolor <strong>sit amet</strong>, consectetur adipiscing
+            elit. Nullam dolor sapien, vulputate eu diam at, condimentum
+            hendrerit tellus. Nam facilisis sodales felis, pharetra pharetra
+            lectus auctor sed. Ut venenatis mauris vel libero pretium, et
+            pretium ligula faucibus. Morbi nibh felis, elementum a posuere et,
+            vulputate et erat. Nam venenatis.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
