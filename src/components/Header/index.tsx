cd07a59b33dc7947e0c2e6506a493ecd8a22841c
Breno Angelo <brenoangeloal@gmail.com>
2022-04-14 23:00:40 -0300

    header done and starting to style home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss'
+import Image from 'next/image'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Image 
+          src="/images/logo.svg" 
+          alt="spacetraveling" 
+          width={238}
+          height={25.63}
+        />
+      </div>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -24,9 +26,50 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+const postsList = [
+  {
+    title: 'Como Utilizar hooks',
+    content: 'Pensando em sincronização em vez de ciclos de vida.',
+    date: '15 Mar 2021',
+    author: 'Joseph Oliveira',
+  },
+  {
+    title: 'Como Utilizar hooks',
+    content: 'Pensando em sincronização em vez de ciclos de vida.',
+    date: '15 Mar 2021',
+    author: 'Joseph Oliveira',
+  },
+  {
+    title: 'Como Utilizar hooks',
+    content: 'Pensando em sincronização em vez de ciclos de vida.',
+    date: '15 Mar 2021',
+    author: 'Joseph Oliveira',
+  },
+  {
+    title: 'Como Utilizar hooks',
+    content: 'Pensando em sincronização em vez de ciclos de vida.',
+    date: '15 Mar 2021',
+    author: 'Joseph Oliveira',
+  },
+];
+
+export default function Home() {
+  // TODO
+  return (
+    <main>
+      {postsList.map(post => (
+        <article>
+          <h1>{post.title}</h1>
+          <p>{post.content}</p>
+          <span>
+            <time><FiCalendar size={20}/> {post.date}</time>
+            <span><FiUser size={20}/> {post.author}</span>
+          </span>
+        </article>
+      ))}
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
